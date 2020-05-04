ce187b27a62bc9f522a63572b7eeb6d3ad6c31d0
elvinos <aep.charles@gmail.com>
2020-05-04 12:08:06 +0100

    Updates

--- docker_push.sh
+++ docker_push.sh
@@ -1,9 +1,7 @@
-
 #!/bin/bash
 
 #echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 #docker push USER/REPO
-
 
 docker ps
 docker container ls
@@ -12,11 +10,6 @@ sudo docker tag webapp-dpdth:latest registry.heroku.com/gmapsapp/web
 sudo docker inspect --format='{{.Id}}' registry.heroku.com/gmapsapp/web
 if [ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then sudo docker push registry.heroku.com/gmapsapp/web; fi
 
-#    - heroku addons:create heroku-postgresql:hobby-dev -a mywebapp0
-
-#    - heroku run container:release web -a webapp-dpdth
-#    - heroku run python manage.py makemigrations -a webapp-dpdth
-#    - heroku run python manage.py migrate -a webapp-dpdth
 chmod +x heroku-container-release.sh
 
 sudo chown $USER:docker ~/.docker
