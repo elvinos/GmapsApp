954982cd4fa249f5fa221f21b36ab7c1342dcb9a
elvinos <aep.charles@gmail.com>
2020-05-04 09:57:13 +0100

    Creation of framework connecting VUE and Django on Docker

---
+++ frontend/src/components/ExampleComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h1>Cookiecutter Django Vue</h1>
+    <p align="center">
+      <img src="https://i.imgur.com/SA8cjs8.png">
+    </p>
+    <h1>Testing</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FirstComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+  h1 {
+    text-align: center;
+  }
+</style>
