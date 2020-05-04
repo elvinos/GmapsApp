1a7e2840d1f040d682a861402ac6f543aa114ef2
elvinos <aep.charles@gmail.com>
2020-05-04 12:26:10 +0100

    Test

--- docker_push.sh
+++ docker_push.sh
@@ -6,7 +6,7 @@
 docker ps
 docker container ls
 sudo docker login --username $HEROKU_DOCKER_USERNAME --password $HEROKU_AUTH_TOKEN registry.heroku.com
-sudo docker tag gmapsapp:latest registry.heroku.com/gmapsapp/web
+sudo docker tag gmapsapp_backend:latest registry.heroku.com/gmapsapp/web
 sudo docker inspect --format='{{.Id}}' registry.heroku.com/gmapsapp/web
 if [ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then sudo docker push registry.heroku.com/gmapsapp/web; fi
 
